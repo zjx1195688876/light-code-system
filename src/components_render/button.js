f2d3fd8e964cca262149907e07df168f3641bd59
zjx1195688876 <15757116071@163.com>
2019-10-05 18:27:26 +0800

    [components_render] 删除不必要的components_render文件夹

--- src/components_render/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'antd';
-
-const renderButton = props => {
-  return <Button type={props.type}>{props.text}</Button>;
-};
-
-export default renderButton;
